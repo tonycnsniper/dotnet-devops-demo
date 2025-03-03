c37c7d04f6336bae16f011335635eb8823df4a93
tony <tony_sniper@163.com>
2025-03-03 22:24:42 +1300

    add a modal used to test post api for creating a threater

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Navbar from "./Navbar";
+import Modal from "./Modal";
 
 type Props = {
   children: ReactNode;
@@ -13,6 +14,10 @@ export default function Layout({ children }: Props) {
         <div className="inner container max-w-screen-md mx-auto pt-2">
           {children}
         </div>
+        <div className="flex items-center justify-center h-screen">
+          <button className="py-2 px-6 bg-blue-500 text-white rounded hover:bg-blue-700 transition font-medium duration-500">SHOW MODAL</button>
+        </div>
+        <Modal />
       </div>
     </>
   );
